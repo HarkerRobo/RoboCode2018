4b5bc8dc48fec70c8f03fde8c566175f23f36230
bokken12 <stopwatchj@gmail.com>
2018-01-13 15:57:16 -0800

    Remove Unnecessary Imports

--- src/org/usfirst/frc/team1072/robot/Robot.java
+++ src/org/usfirst/frc/team1072/robot/Robot.java
@@ -8,10 +8,7 @@
 package org.usfirst.frc.team1072.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.team1072.robot.subsystems.Drivetrain;
 
 /**
