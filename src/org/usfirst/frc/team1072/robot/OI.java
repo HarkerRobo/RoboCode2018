56f588bdfe5931e0b662a647c45a7be62e26a1e4
bokken12 <stopwatchj@gmail.com>
2018-01-20 11:57:19 -0800

    Bind Commands to Gamepad

--- src/org/usfirst/frc/team1072/robot/OI.java
+++ src/org/usfirst/frc/team1072/robot/OI.java
@@ -8,6 +8,10 @@
 
 package org.usfirst.frc.team1072.robot;
 
+import org.usfirst.frc.team1072.harkerrobolib.wrappers.GamepadWrapper;
+import org.usfirst.frc.team1072.robot.commands.EjectCommand;
+import org.usfirst.frc.team1072.robot.commands.TestIntakeCommand;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
@@ -40,4 +44,11 @@ public class OI {
 	// Start the command when the button is released and let it run the command
 	// until it is finished as determined by it's isFinished method.
 	// button.whenReleased(new ExampleCommand());
+	
+	public static final GamepadWrapper gamepad = new GamepadWrapper(0);
+	
+	public static void initializeCommandBindings() {
+		gamepad.getButtonBumperLeft().whenPressed(new TestIntakeCommand());
+		gamepad.getButtonBumperRight().whenPressed(new EjectCommand());
+	}
 }
--- src/org/usfirst/frc/team1072/robot/commands/TestIntakeCommand.java
+++ src/org/usfirst/frc/team1072/robot/commands/TestIntakeCommand.java
@@ -1,7 +1,5 @@
 package org.usfirst.frc.team1072.robot.commands;
 
-import java.io.ObjectOutputStream.PutField;
-
 import org.usfirst.frc.team1072.robot.Robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
@@ -13,37 +11,37 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  *
  */
 public class TestIntakeCommand extends Command {
-
-    public static final double STALL_CURRENT = 18;
-    
-    public TestIntakeCommand() {
-        requires(Robot.gears);
-    }
-
-    // Called just before this Command runs the first time
-    protected void initialize() {
-        Robot.gears.getRollers().set(ControlMode.PercentOutput, 0.4);
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-        SmartDashboard.putNumber("intake current", Robot.gears.getRollers().getOutputCurrent());
-        SmartDashboard.putNumber("Encoder value", Robot.gears.getOrientation().getSelectedSensorVelocity(0));
-        SmartDashboard.putNumber("Encoder position", Robot.gears.getOrientation().getSelectedSensorPosition(0));
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return Robot.gears.getRollers().getOutputCurrent() > STALL_CURRENT;
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-        Robot.gears.getRollers().set(ControlMode.PercentOutput, 0);
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	
+	public static final double STALL_CURRENT = 18;
+	
+	public TestIntakeCommand() {
+		requires(Robot.gears);
+	}
+	
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		Robot.gears.getRollers().set(ControlMode.PercentOutput, 0.4);
+	}
+	
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		SmartDashboard.putNumber("intake current", Robot.gears.getRollers().getOutputCurrent());
+		SmartDashboard.putNumber("Encoder value", Robot.gears.getOrientation().getSelectedSensorVelocity(0));
+		SmartDashboard.putNumber("Encoder position", Robot.gears.getOrientation().getSelectedSensorPosition(0));
+	}
+	
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return Robot.gears.getRollers().getOutputCurrent() > STALL_CURRENT;
+	}
+	
+	// Called once after isFinished returns true
+	protected void end() {
+		Robot.gears.getRollers().set(ControlMode.PercentOutput, 0);
+	}
+	
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
 }
