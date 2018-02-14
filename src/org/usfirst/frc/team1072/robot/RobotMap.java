615348df7c6cd9e5b0100d7864d33c6e0c391d6a
bokken12 <stopwatchj@gmail.com>
2018-02-14 14:28:56 -0800

    Update Solenoid IDs

--- src/org/usfirst/frc/team1072/robot/RobotMap.java
+++ src/org/usfirst/frc/team1072/robot/RobotMap.java
@@ -54,7 +54,7 @@ public class RobotMap {
 		 * On each side of the intake, a solenoid can extend to shrink the
 		 * intake area
 		 */
-		public static final int EXPANSION_SOLENOID_A = 0, EXPANSION_SOLENOID_B = 1, RAISING_SOLENOID_A = 2, RAISING_SOLENOID_B = 3;
+		public static final int EXPANSION_SOLENOID_A = 0, EXPANSION_SOLENOID_B = 2, RAISING_SOLENOID_A = 1, RAISING_SOLENOID_B = 3;
 	}
 	
 //	public static class GearIntake {
